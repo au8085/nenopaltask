02117a0762c2b9ae4d459f88bf6a79aeada40cb5
au8085 <upadhyay.aditya8085@gmail.com>
2024-03-08 12:13:46 +0530

    form module update

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -9,6 +9,7 @@ import {
   Label,
   Input,
 } from "reactstrap";
+import "./modal.css";
 
 const ModalComponent = ({ isOpen, closeModal, onSave, initialValue }) => {
   const [userData, setUserData] = useState(initialValue || {});
@@ -31,56 +32,147 @@ const ModalComponent = ({ isOpen, closeModal, onSave, initialValue }) => {
   };
 
   return (
+    // <Modal isOpen={isOpen} backdrop={true}>
+    //   <ModalHeader toggle={closeModal}>Edit User Details</ModalHeader>
+    //   <ModalBody>
+    //     <FormGroup>
+    //       <Label for="name">Name:</Label>
+    //       <Input
+    //         type="text"
+    //         id="name"
+    //         name="name"
+    //         value={userData?.name}
+    //         onChange={handleChange}
+    //       />
+
+    //       <Label for="email">Email:</Label>
+    //       <Input
+    //         type="text"
+    //         id="email"
+    //         name="email"
+    //         value={userData?.email}
+    //         onChange={handleChange}
+    //       />
+
+    //       <Label for="phone">Phone:</Label>
+    //       <Input
+    //         type="text"
+    //         id="phone"
+    //         name="phone"
+    //         value={userData?.phone}
+    //         onChange={handleChange}
+    //       />
+
+    //       <Label for="website">Website:</Label>
+    //       <Input
+    //         type="text"
+    //         id="website"
+    //         name="website"
+    //         value={userData?.website}
+    //         onChange={handleChange}
+    //       />
+    //     </FormGroup>
+    //   </ModalBody>
+    //   <ModalFooter>
+    //     <Button color="primary" onClick={handleSave}>
+    //       Ok
+    //     </Button>{" "}
+    //     <Button color="secondary" onClick={closeModal}>
+    //       Cancel
+    //     </Button>
+    //   </ModalFooter>
+    // </Modal>
     <Modal isOpen={isOpen} backdrop={true}>
-      <ModalHeader toggle={closeModal}>Edit User Details</ModalHeader>
-      <ModalBody>
-        <FormGroup>
-          <Label for="name">Name:</Label>
-          <Input
-            type="text"
-            id="name"
-            name="name"
-            value={userData?.name}
-            onChange={handleChange}
-          />
+    <ModalHeader toggle={closeModal}>Edit User Details</ModalHeader>
+    <ModalBody>
+      <FormGroup>
+        <div className="row mb-3">
+          <div className="col-md-3">
+            <Label for="name" className="form-label">
+            <span className="required">*</span> Name:
+            </Label>
+          </div>
+          <div className="col-md-9">
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              value={userData?.name}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
+        </div>
 
-          <Label for="email">Email:</Label>
-          <Input
-            type="text"
-            id="email"
-            name="email"
-            value={userData?.email}
-            onChange={handleChange}
-          />
+        <div className="row mb-3">
+          <div className="col-md-3">
+            <Label for="email" className="form-label">
+            <span className="required">*</span> Email:
+            </Label>
+          </div>
+          <div className="col-md-9">
+            <Input
+              type="text"
+              id="email"
+              name="email"
+              value={userData?.email}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
+        </div>
 
-          <Label for="phone">Phone:</Label>
-          <Input
-            type="text"
-            id="phone"
-            name="phone"
-            value={userData?.phone}
-            onChange={handleChange}
-          />
+        <div className="row mb-3">
+          <div className="col-md-3">
+            <Label for="phone" className="form-label">
+            <span className="required">*</span> Phone:
+            </Label>
+          </div> 
+          <div className="col-md-9">
+            <Input
+              type="text"
+              id="phone"
+              name="phone"
+              value={userData?.phone}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
+        </div>
 
-          <Label for="website">Website:</Label>
-          <Input
-            type="text"
-            id="website"
-            name="website"
-            value={userData?.website}
-            onChange={handleChange}
-          />
-        </FormGroup>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="primary" onClick={handleSave}>
-          Ok
-        </Button>{" "}
-        <Button color="secondary" onClick={closeModal}>
-          Cancel
-        </Button>
-      </ModalFooter>
-    </Modal>
+        <div className="row mb-3">
+          <div className="col-md-3">
+            <Label for="website" className="form-label">
+            <span className="required">*</span> Website:
+            </Label>
+          </div>
+          <div className="col-md-9">
+            <Input
+              type="text"
+              id="website"
+              name="website"
+              value={userData?.website}
+              onChange={handleChange}
+              className="form-control"
+            />
+          </div>
+        </div>
+      </FormGroup>
+    </ModalBody>
+    <ModalFooter>
+      <Button color="primary" onClick={handleSave}>
+        Ok
+      </Button>{" "}
+      <Button color="secondary" onClick={closeModal}>
+        Cancel
+      </Button>
+    </ModalFooter>
+  </Modal>
+   
+   
   );
 };
 
